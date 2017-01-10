31420976dbb8c224cfbbf286e01596e0f24113cd
Albert Alma <albert.alma@king.com>
2017-01-10 22:32:34 +0100

    ship pointing to mouse cursor

--- Assets/Scripts/MainSpaceShip/SpaceShipController.cs
+++ Assets/Scripts/MainSpaceShip/SpaceShipController.cs
@@ -40,10 +40,21 @@ public class SpaceShipController : MonoBehaviour {
 		shipRigidbody.velocity = new Vector2(Mathf.Clamp(shipRigidbody.velocity.x, -maxSpeed, maxSpeed),
 			Mathf.Clamp(shipRigidbody.velocity.y, -maxSpeed, maxSpeed));
 
-		if (horizontal != 0 || vertical != 0) {
-			Debug.Log ("h: " + horizontal);
-			Debug.Log ("v: " + vertical);
-		}
+		//Update rotation - turn to face mouse pointer
+		//Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, 0.0f));
+		//Debug.Log (mousePosWorld);
+		//Get direction to cursor
+		//Vector3 LookDirection = mousePosWorld - shipTransform.position;
 
+		//shipTransform.localRotation = Quaternion.LookRotation(LookDirection.normalized,Vector3.forward);
+
+		Vector3 mousePosition = Input.mousePosition;
+		mousePosition.z = Camera.main.transform.position.z - shipTransform.position.z;
+		Vector3 objectPosition = Camera.main.WorldToScreenPoint(shipTransform.position);
+		mousePosition.x = mousePosition.x - objectPosition.x;
+		mousePosition.y = mousePosition.y - objectPosition.y;
+		float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+		shipTransform.rotation = Quaternion.Euler (0.0f, 0.0f, angle);
 	}
+		
 }
