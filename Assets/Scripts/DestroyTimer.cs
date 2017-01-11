fa8e1f99ac6f3a921e5a1ecd05d1f55bef0fcf22
Albert Alma <albert.alma@king.com>
2017-01-11 23:22:22 +0100

    added health script and particle system for death

---
+++ Assets/Scripts/DestroyTimer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTimer : MonoBehaviour {
+
+	public float timeUntilDestroy = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Destroy", timeUntilDestroy);
+	}
+
+	void Destroy() {
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	[SerializeField]
+	private float _healthPoints = 100f;
+	
+	public GameObject deathParticlesPrefab = null;
+	private Transform thisTransform = null;
+	public bool shouldDestroyOnDeath = true;
+
+	void Start() {
+		thisTransform = GetComponent<Transform>();
+	}
+
+	public float HealthPoints {
+		get {
+			return _healthPoints;
+		}
+
+		set {
+			_healthPoints = value;
+
+			if(_healthPoints <= 0)
+			{
+				SendMessage("Destroy", SendMessageOptions.DontRequireReceiver);
+
+				if(deathParticlesPrefab != null)
+					Instantiate(deathParticlesPrefab, thisTransform.position, thisTransform.rotation);
+
+				if(shouldDestroyOnDeath)
+					Destroy(gameObject);
+			}
+		}
+	}
+
+
+	//Only for test purpose
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			HealthPoints = 0;
+	}
+}
