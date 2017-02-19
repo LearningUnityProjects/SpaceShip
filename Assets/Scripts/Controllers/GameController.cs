0312ac033aaebd9e560947344ea99036dc9ff0d9
Albert Alma <albert.alma@king.com>
2017-02-19 21:49:57 +0100

    last commit

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	//Game score
+	public static int score;
+
+	//Prefix
+	public string scorePrefix = string.Empty;
+
+	public Text scoreText = null;
+
+	public Text gameOverText = null;
+
+	public static GameController instance = null;
+
+	void Awake() {
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (scoreText != null) {
+			scoreText.text = scorePrefix + score.ToString();
+		}
+	}
+
+	public static void GameOver() {
+		if (instance.gameOverText != null) {
+			instance.gameOverText.gameObject.SetActive (true);
+		}
+	}
+}
---
+++ Assets/Scripts/Enemies/ScoreOnDestroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreOnDestroy : MonoBehaviour {
+
+	public int scoreValue = 50;
+
+	void OnDestroy() {
+		GameController.score += scoreValue;
+	}
+}
